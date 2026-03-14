4d2a54055a0737514519a5aeea8ce8c6f3f27e33
Jorge Alejandro Casierra <jorjecasanova@gmail.com>
2026-03-14 12:29:31 -0500

    fix: add app_name to users urls to support namespace in include()

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('', views.UserListCreateView.as_view(), name='user-list-create'),
     path('<int:user_id>/', views.UserDetailView.as_view(), name='user-detail'),
