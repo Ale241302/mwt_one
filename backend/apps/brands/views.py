9dd76a816987fa7960cf0ba54112ec783bc86b6f
Jorge Alejandro Casierra <jorjecasanova@gmail.com>
2026-03-17 15:16:03 -0500

    fix: brands view allow POST (ListCreateAPIView)

--- backend/apps/brands/views.py
+++ backend/apps/brands/views.py
@@ -1,7 +1,10 @@
-﻿from rest_framework import generics
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Brand
 from .serializers import BrandSerializer
 
-class BrandListView(generics.ListAPIView):
+
+class BrandListView(generics.ListCreateAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    permission_classes = [IsAuthenticated]
