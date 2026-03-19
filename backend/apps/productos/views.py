2b8fd00fe1607886d966fcd0f5212a0f7dfe7e6c
Ale241302 <jorjecasanova@gmail.com>
2026-03-19 13:54:06 -0500

    cambios varios 20260319

--- backend/apps/productos/views.py
+++ backend/apps/productos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Producto
 from .serializers import ProductoSerializer
@@ -10,3 +11,8 @@ class ProductoViewSet(viewsets.ModelViewSet):
     filterset_fields = ['brand', 'category']
     search_fields = ['name', 'sku_base', 'description']
     ordering_fields = ['name', 'created_at']
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data, 'count': queryset.count()})
